76f844f4f17f0af0409babeef7f86ffcd4b7b129
fomgleb <fomglib@gmail.com>
2025-07-04 23:07:26 +0300

    Refactor Renderable.zig interface

--- src/Renderable.zig
+++ src/Renderable.zig
@@ -1,20 +1,19 @@
 const Renderable = @This();
 
 context: *const anyopaque,
-render_fn: *const fn (self: *const anyopaque) error{SdlError}!void,
+renderFn: *const fn (self: *const anyopaque) error{SdlError}!void,
 
 pub fn init(context: anytype) Renderable {
     return Renderable{
         .context = context,
-        .render_fn = struct {
+        .renderFn = struct {
             pub fn render(pointer: *const anyopaque) error{SdlError}!void {
-                const self: @TypeOf(context) = @ptrCast(@alignCast(pointer));
-                return @typeInfo(@TypeOf(context)).pointer.child.render(self);
+                return @typeInfo(@TypeOf(context)).pointer.child.render(@ptrCast(@alignCast(pointer)));
             }
         }.render,
     };
 }
 
 pub fn render(self: Renderable) error{SdlError}!void {
-    try self.render_fn(self.context);
+    try self.renderFn(self.context);
 }
