9a484510cb03a0463adf2381b89d935ef20f7452
fomgleb <fomglib@gmail.com>
2025-07-04 20:06:20 +0300

    Add cast functions for Point struct

--- src/point.zig
+++ src/point.zig
@@ -1,6 +1,20 @@
 pub fn Point(T: type) type {
     return struct {
+        const Self = @This();
+
         x: T,
         y: T,
+
+        pub fn as(self: Self, NewType: type) Point(NewType) {
+            return Point(NewType){ .x = self.x, .y = self.y };
+        }
+
+        pub fn intCast(self: Self, NewType: type) Point(NewType) {
+            return Point(NewType){ .x = @intCast(self.x), .y = @intCast(self.y) };
+        }
+
+        pub fn floatFromInt(self: Self, NewType: type) Point(NewType) {
+            return Point(NewType){ .x = @floatFromInt(self.x), .y = @floatFromInt(self.y) };
+        }
     };
 }
