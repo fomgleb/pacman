d22f557bb11651ea35033dc9cc8f300b56f415b7
fomgleb <fomglib@gmail.com>
2025-08-03 13:21:11 +0300

    Fix bug: strange pacman sprite at the start

--- src/ecs/component/MovementAnimation.zig
+++ src/ecs/component/MovementAnimation.zig
@@ -6,7 +6,7 @@ sprite_sheet: *c.SDL_Texture,
 sprite_size: Vec2(f32),
 fps: f32,
 sprite_can_rotate: bool,
-current_frame_index: f32 = -1,
+current_frame_index: f32 = 0,
 sprite_sheet_read_direction: enum { right, left } = .right,
 
 pub fn init(sprite_sheet: *c.SDL_Texture, sprite_width: f32, fps: f32, sprite_can_rotate: bool) !@This() {
